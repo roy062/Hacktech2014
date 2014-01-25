644251eba953ba311d45bada18f4712000602371
dkho <daniel.j.kho@gmail.com>
2014-01-25 08:19:09 -0800

    Potentially More functional
    Try it! I bet it'll fail...

--- src/com/ripecho/hacktech2014/PixelGridView.java
+++ src/com/ripecho/hacktech2014/PixelGridView.java
@@ -9,29 +9,28 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Rect;
 
 public class PixelGridView extends View {
-
 	private DrawActivity parent;
-	private Display display;
-	private int cnvHeight;
-	private int cnvWidth;
-	private Bitmap bitmap;
 	private Paint linePaint;
 	private Paint toolPaint;
 	private Paint bgPaint;
+	private int szX;
+	private int szY;
+	private int numPxX;
+	private int numPxY;
+
 	
 	public PixelGridView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		
-		Point scrnSize = new Point();
-		display.getSize(scrnSize);
-		cnvHeight = (int)(scrnSize.y - 0.15*scrnSize.y);
-		cnvWidth = scrnSize.x;
-		bitmap = parent.getBitmap();
 		linePaint.setColor(Color.BLACK);
+		linePaint.setStrokeWidth(0);
 		toolPaint.setColor(parent.getColor());
 		bgPaint.setColor(Color.GRAY);
+		setBackgroundColor(Color.GRAY);
+		setDrawingArea(32,32);
 		
 	}
 
@@ -39,12 +38,21 @@ public class PixelGridView extends View {
 		parent = p;
 	}
 	
+	private void setDrawingArea(int x, int y){
+		numPxX = x;
+		numPxY = y;
+		szX = getWidth()/x;
+		if(szX*y > getHeight()) szY = szX = getHeight()/y;
+		else szY = szX;
+	}
+	
 	protected void onDraw(Canvas canvas){
 		super.onDraw(canvas);
-		canvas.drawRect(0,0,cnvWidth,cnvWidth/4, bgPaint);
-		canvas.drawRect(0,3*cnvWidth/4,cnvWidth,cnvHeight,bgPaint);
-		if(isDirty()){
-			
+		for(int i = szX; i < getWidth(); i += szX){
+			canvas.drawLine(i, 0, i, getHeight(), linePaint);
+		}
+		for(int i = szY; i < getHeight(); i += szY){
+			canvas.drawLine(0, i, getWidth(), i, linePaint);
 		}
 	}
 	
