9844bfd1f05d6adaf102e94a80798b293eef5159
Roy Xia <roy062@gmail.com>
2014-01-25 00:53:01 -0800

    Reorganized setup a bit: the main activity is now DrawActivity

---
+++ src/com/ripecho/hacktech2014/DrawActivity.java
@@ -0,0 +1,12 @@
+package com.ripecho.hacktech2014;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrawActivity extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState)
+	{
+		
+	}
+}
--- src/com/ripecho/hacktech2014/PixelMapView.java
+++
@@ -1,5 +0,0 @@
-package com.ripecho.hacktech2014;
-
-public class PixelMapView {
-
-}
