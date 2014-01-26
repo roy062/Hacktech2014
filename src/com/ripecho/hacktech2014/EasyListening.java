1ab3b4ea255f2e55d9859163c863335aa979485c
unknown <iyalsuresh@ksuresh-THINK.hq.storsimple.com>
2014-01-25 19:48:46 -0800

    summary :P, now with less stupidity

--- src/com/ripecho/hacktech2014/EasyListening.java
+++ src/com/ripecho/hacktech2014/EasyListening.java
@@ -1,5 +1,21 @@
 package com.ripecho.hacktech2014;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 public class EasyListening {
 
+	
+	public void drawBMP(Canvas c, Bitmap bmp){
+		Paint paint = new Paint();
+		//paint.setColor(parent.getColor());
+		for(int i =0; i< numPxY;i++){
+			for(int j =0; j<numPxX;j++){
+				paint.setColor(bmp.getPixel(j, i));
+				c.drawRect(j, i, j+szX, i+szY, paint);
+			}
+		}
+	}
+	
 }
