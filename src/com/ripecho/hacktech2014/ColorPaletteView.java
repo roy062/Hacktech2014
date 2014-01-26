ceef39426918dc8c2c8de8dca86e9a90151f7ec9
Roy Xia <roy062@gmail.com>
2014-01-26 03:20:23 -0800

    random button to get new colors

--- src/com/ripecho/hacktech2014/ColorPaletteView.java
+++ src/com/ripecho/hacktech2014/ColorPaletteView.java
@@ -1,21 +1,28 @@
 package com.ripecho.hacktech2014;
 
+import java.util.Timer;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.Display;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.PopupMenu;
+import android.widget.SeekBar;
 
 
-public class ColorPaletteView extends View implements View.OnClickListener, View.OnLongClickListener {
+public class ColorPaletteView extends View implements GestureDetector.OnGestureListener {
 
 	public static final int MAX_PALETTE_COLORS = 16;
 	int currentIndex = 0;
@@ -26,7 +33,10 @@ public class ColorPaletteView extends View implements View.OnClickListener, View
 	private Paint color;
 	ColorWheelDialog cwheel;
 	private int lastColorIndex;
+	private int finalizedColorIndex;
 	private PopupMenu colorMenu;
+	private Timer t;
+	private Bitmap randomButton;
 
 	private int[] paletteColors = new int[MAX_PALETTE_COLORS];
 	
@@ -46,9 +56,9 @@ public class ColorPaletteView extends View implements View.OnClickListener, View
 		display.getSize(size);
 		dispWidth = size.x;
 		dispHeight = size.y;
-		this.setOnClickListener(this);
 		parent = (DrawActivity)context;
 		isExpanded = true;
+		randomButton = BitmapFactory.decodeResource(parent.getResources(), R.drawable.random);
 	}
 	
 	public void setParent(DrawActivity parent){
@@ -71,7 +81,10 @@ public class ColorPaletteView extends View implements View.OnClickListener, View
 				color.setColor(paletteColors[i]);
 				paint.drawRect(i*getWidth()/8, 0, (i+1)*getWidth()/8, getHeight()/2, color);
 				color.setColor(paletteColors[i+8]);
-				paint.drawRect(i*getWidth()/8, getHeight()/2, (i+1)*getWidth()/8, getHeight(), color);
+				if (i != 7)
+					paint.drawRect(i*getWidth()/8, getHeight()/2, (i+1)*getWidth()/8, getHeight(), color);
+				else
+					paint.drawBitmap(randomButton, new Rect(0, 0, 1080, 1080), new Rect(7 * getWidth() / 8, getHeight()/2, getWidth(), getHeight()), color);
 			}
 		//}
 	}
@@ -82,8 +95,13 @@ public class ColorPaletteView extends View implements View.OnClickListener, View
 		//{
 		lastColorIndex = getPaletteIndex(e);
 			
-	
-			if(paletteColors[lastColorIndex]!=Color.TRANSPARENT)
+			if(lastColorIndex == 15) // randomize
+			{
+				for (int i = 0; i < 15; i++)
+					paletteColors[i] = 0xFF000000 + (int)(Math.random() * 0x00FFFFFF);
+				invalidate();
+			}
+			else if (paletteColors[lastColorIndex]!=Color.TRANSPARENT)
 			{
 				parent.setColor(paletteColors[lastColorIndex]);
 			}
@@ -99,10 +117,20 @@ public class ColorPaletteView extends View implements View.OnClickListener, View
 		//}
 		//else
 			//isExpanded = true;
+		/*if (e.getAction() == MotionEvent.ACTION_DOWN && t == null)
+		{
+			t = new Timer();
+			t.schedule(new TimerTask() { public void run() { onLongClick(null); } }, 1000);
+		}
+		else if (e.getAction() == MotionEvent.ACTION_UP && t != null)
+		{
+			t.cancel();
+			t = null;
+		}*/
 		return true;
 	}
 	
-	public boolean onLongClick(View click){
+	public void onLongPress(MotionEvent e){
 		//if(isExpanded){
 			//cwheel.show(cwheel.getFragmentManager(),"ColorWheel");
 			//isColorWheel = true;
@@ -113,10 +141,14 @@ public class ColorPaletteView extends View implements View.OnClickListener, View
 		AlertDialog.Builder builder = new AlertDialog.Builder(parent);
 		builder.setTitle(R.string.dialog_title);
 		builder.setView(parent.getLayoutInflater().inflate(R.layout.color_slider, null));
+		finalizedColorIndex = lastColorIndex;
 		
 		builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
 	           public void onClick(DialogInterface dialog, int id) {
-	               // User clicked OK button
+	        	  int r = ((SeekBar)findViewById(R.id.red)).getThumbOffset();
+		       		int g = ((SeekBar)findViewById(R.id.green)).getThumbOffset();
+		       		int b = ((SeekBar)findViewById(R.id.blue)).getThumbOffset();
+		       		paletteColors[finalizedColorIndex] = 0xFF000000 + r << 4 + g << 2 + b;
 	           }
 	       });
 		builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -126,7 +158,9 @@ public class ColorPaletteView extends View implements View.OnClickListener, View
 	       });
 		
 		AlertDialog dialog = builder.create();
-		return true;
+		((SeekBar)dialog.findViewById(R.id.red)).setThumbOffset(paletteColors[finalizedColorIndex] & 0x00FF0000 >> 4);
+		((SeekBar)dialog.findViewById(R.id.green)).setThumbOffset(paletteColors[finalizedColorIndex] & 0x0000FF00 >> 2);
+		((SeekBar)dialog.findViewById(R.id.blue)).setThumbOffset(paletteColors[finalizedColorIndex] & 0x000000FF);
 	}
 	
 	private int getPaletteIndex(MotionEvent e){
@@ -145,9 +179,35 @@ public class ColorPaletteView extends View implements View.OnClickListener, View
 	}
 
 	@Override
-	public void onClick(View v) {
+	public boolean onDown(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	//@Override
